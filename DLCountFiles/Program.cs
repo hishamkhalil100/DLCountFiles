e22d73cbb57e8c662eb08eaa6639777656b14d9e
heshamkhalil <h_eyg@hotmail.com>
2022-02-20 14:47:33 +0300

    add images support

--- DLCountFiles/Program.cs
+++ DLCountFiles/Program.cs
@@ -14,98 +14,55 @@ namespace DLCountFiles
         //  static OdbcConnection con1 = new OdbcConnection("Driver=Sybase ASE ODBC Driver;SRVR=production;DB=kfnl;UID=sa;PWd=sybase1;");
         static void Main(string[] args)
         {
-            string folderPath = @"\\kfnl-sf01\مخرجات التشغيل\غلافات العناوين - كتب الملك سلمان - 10-11-2021";
-            DirectoryInfo di = new DirectoryInfo(folderPath);
+            using (OdbcConnection con1 = new OdbcConnection("Driver=Sybase ASE ODBC Driver;SRVR=production;DB=kfnl;UID=sa;PWd=sybase1;"))
+            {
+                con1.Open();
+                string folderPath = @"\\kfnl-fs01\g$\TempDLCovers\New folder\7-New\7";
+                DirectoryInfo di = new DirectoryInfo(folderPath);
 
-            
+
                 // This path is a file
-                ProcessDirectory(di);
-        
-            //DirectoryInfo di = new DirectoryInfo(folderPath);
-            //DirectoryInfo[] directoryInfos = di.GetDirectories();
-            //foreach (var dir in directoryInfos)
-            //{
-            //    OdbcConnection con1 = new OdbcConnection("Driver=Sybase ASE ODBC Driver;SRVR=production;DB=kfnl;UID=sa;PWd=sybase1;");
-            //    FileInfo[] files = dir.GetFiles();
-            //    string s = string.Empty;
-            //    try
-            //    {
+                ProcessDirectory(di, con1);
+            }
 
-            //        con1.Open();
-            //        foreach (FileInfo file in files)
-            //        {
-
-            //            int bibNo = 0;
-            //            s = file.Name;
-            //            //string fileName = file.Name;
-            //            if (file.Name.Contains("-"))
-            //            {
-            //                bibNo = int.Parse(file.Name.Substring(0, file.Name.IndexOf('-')));
-            //            }
-            //            else
-            //            {
-            //                bibNo = int.Parse(file.Name.ToLower().Replace(".pdf", ""));
-            //            }
-            //            try
-            //            {
-            //                Console.WriteLine(bibNo);
-            //                OdbcCommand commandItem = new OdbcCommand(@"insert into DLDEPTCD25112020 (bib#,FileName,FilePath) values (?,?,?)", con1);
-            //                commandItem.Parameters.Add(new OdbcParameter("@bib#", bibNo));
-            //                commandItem.Parameters.Add(new OdbcParameter("@bib#", file.Name));
-            //                commandItem.Parameters.Add(new OdbcParameter("@bib#", Path.GetFullPath(file.DirectoryName)));
-            //                commandItem.ExecuteNonQuery();
-            //            }
-            //            catch
-            //            {
-            //            }
-            //        }
-
-            //    }
-            //    catch (Exception ex)
-            //    {
-            //        Console.WriteLine(ex.Message);
-
-            //    }
-            //    finally
-            //    {
-            //        con1.Dispose();
-            //        con1.Close();
-
-            //    }
-            //}
         }
 
 
         // Process all files in the directory passed in, recurse on any directories 
         // that are found, and process the files they contain.
-        public static void ProcessDirectory(DirectoryInfo targetDirectory)
+        public static void ProcessDirectory(DirectoryInfo targetDirectory, OdbcConnection con1)
         {
+
+
             // Process the list of files found in the directory.
             FileInfo[] fileEntries = targetDirectory.GetFiles();
             foreach (FileInfo fileName in fileEntries)
             {
-               // if (fileName.Extension.Equals(".pdf") || fileName.Extension.Equals(".PDF"))
-                    ProcessFile(fileName);
+                Console.WriteLine($"get File{fileName}");
+                // if (fileName.Extension.Equals(".pdf") || fileName.Extension.Equals(".PDF"))
+                ProcessFile(fileName, con1);
             }
 
             // Recurse into subdirectories of this directory.
             DirectoryInfo[] subdirectoryEntries = targetDirectory.GetDirectories();
             foreach (DirectoryInfo subdirectory in subdirectoryEntries)
-                ProcessDirectory(subdirectory);
+            {
+                Console.WriteLine("get Direct");
+                ProcessDirectory(subdirectory, con1);
+            }
         }
 
         // Insert logic for processing found files here.
-        public static void ProcessFile(FileInfo file)
+        public static void ProcessFile(FileInfo file, OdbcConnection con1)
         {
 
 
-            OdbcConnection con1 = new OdbcConnection("Driver=Sybase ASE ODBC Driver;SRVR=production;DB=kfnl;UID=sa;PWd=sybase1;");
+
 
             string s = string.Empty;
             try
             {
 
-                con1.Open();
 
 
                 int bibNo = 0;
@@ -115,22 +72,30 @@ namespace DLCountFiles
                 {
                     bibNo = int.Parse(file.Name.Substring(0, file.Name.IndexOf('-')));
                 }
+                else if (file.Name.ToLower().Contains("jpg"))
+                {
+                    bibNo = int.Parse(file.Name.ToLower().Replace(".jpg", ""));
+                }
+                else if (file.Name.ToLower().Contains("png"))
+                {
+                    bibNo = int.Parse(file.Name.ToLower().Replace(".png", ""));
+                }
                 else
                 {
                     bibNo = int.Parse(file.Name.ToLower().Replace(".pdf", ""));
                 }
-                try
-                {
-                    Console.WriteLine(bibNo);
-                    OdbcCommand commandItem = new OdbcCommand(@"insert into dbo.tempKingSalman_10_11_2021 (bib#,FileName,FilePath) values (?,?,?)", con1);
-                    commandItem.Parameters.Add(new OdbcParameter("@bib#", bibNo));
-                    commandItem.Parameters.Add(new OdbcParameter("@bib#", file.Name));
-                    commandItem.Parameters.Add(new OdbcParameter("@bib#", Path.GetFullPath(file.DirectoryName)));
-                    commandItem.ExecuteNonQuery();
-                }
-                catch
-                {
-                }
+
+
+
+                Console.WriteLine(bibNo);
+                OdbcCommand commandItem = new OdbcCommand(@"insert into dbo.tempDlCoversFromAbst (bib#,FileName,FilePath) values (?,?,?)", con1);
+                commandItem.Parameters.Add(new OdbcParameter("@bib#", bibNo));
+                commandItem.Parameters.Add(new OdbcParameter("@bib#", file.Name));
+                commandItem.Parameters.Add(new OdbcParameter("@bib#", Path.GetFullPath(file.DirectoryName)));
+                commandItem.ExecuteNonQuery();
+                Console.WriteLine("Finish!");
+
+
 
 
             }
@@ -139,12 +104,7 @@ namespace DLCountFiles
                 Console.WriteLine(ex.Message);
 
             }
-            finally
-            {
-                con1.Dispose();
-                con1.Close();
 
-            }
         }
     }
 }
